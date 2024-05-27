250d26037cf500e00c383dc128a0fc80d2015bfe
Joy Brar <joybrar2001@gmail.com>
2024-05-27 17:39:45 +0530

    Add Rank

--- src/App.jsx
+++ src/App.jsx
@@ -44,66 +44,65 @@ const App = () => {
 
   return (
     <>
-      <header className='font-googleSans h-12 flex justify-center items-center'>
-        <h1 className='font-bold'>Main Leaderboard</h1>
+      <header class='font-googleSans h-12 flex justify-center items-center'>
+        <h1 class='font-bold text-center text-lg sm:text-xl'>Main Leaderboard</h1>
       </header>
-      <div className='w-full'>
-        <img src="Google-Pay-banner 1.png" alt="Google Pay Banner" className='z-10' />
+      <div class='w-full'>
+        <img src="Google-Pay-banner 1.png" alt="Google Pay Banner" class='w-full' />
       </div>
-      <main className='font-googleSans flex flex-col w-full -mt-2 z-10 bg-white rounded-xl overflow-hidden pb-16'>
-        {tableData && tableData.length > 0 && (
-          <table className='min-w-full divide-y divide-gray-200'>
-            <thead className='bg-white w-full'>
+      <main class='font-googleSans flex flex-col w-full -mt-2 z-10 bg-white rounded-xl overflow-hidden pb-16'>
+        <div class='overflow-x-auto'>
+          <table class='min-w-full divide-y divide-gray-200'>
+            <thead class='bg-white'>
               <tr>
-                <th className='px-6 py-3 text-left text-sm font-bold tracking-wider'>
+                <th class='px-2 sm:px-6 py-3 text-left text-xs sm:text-sm font-bold tracking-wider'>
                   Rank
                 </th>
-                <th className='px-6 py-3 text-left text-sm font-bold tracking-wider'>
+                <th class='px-2 py-1 sm:px-6 sm:py-3 text-left text-xs sm:text-sm font-bold tracking-wider'>
                   Territory
                 </th>
-                <th className='px-6 py-3 text-left text-sm font-bold tracking-wider'>
+                <th class='px-2 py-1 sm:px-6 sm:py-3 text-left text-xs sm:text-sm font-bold tracking-wider'>
                   Points
                 </th>
               </tr>
             </thead>
-            <tbody className='bg-white divide-y divide-gray-200'>
+            <tbody class='bg-white divide-y divide-gray-200'>
               {tableData.map((entry, index) => (
-                <tr key={index} className='hover:bg-gray-100'>
-                  <td className='px-6 py-4 whitespace-nowrap'>
-                    <div className='text-sm text-gray-900'>
+                <tr key={index} class='hover:bg-gray-100'>
+                  <td class='pl-4 sm:px-6 py-4 whitespace-nowrap'>
+                    <div class='text-xs sm:text-sm text-gray-900'>
                       {index + 1}
                       {index === 0 ? 'st' : index === 1 ? 'nd' : index === 2 ? 'rd' : 'th'}
                     </div>
                   </td>
-                  <td className='px-6 py-4 whitespace-nowrap'>
-                    <div className='text-sm text-gray-900'>{entry.territory_id}</div>
+                  <td class='px-2 py-1 sm:px-6 sm:py-4 whitespace-nowrap'>
+                    <div class='text-xs sm:text-sm text-gray-900'>{entry.territory_id}</div>
                   </td>
-                  <td className='px-6 py-4 whitespace-nowrap flex gap-1 justify-start items-center'>
-                    <img src="icon 2.png" alt="" className='h-4 w-4' />
-                    <div className='text-sm text-gray-900'>{entry.total_points}</div>
+                  <td class='pr-4 py-1 sm:px-6 sm:py-4 whitespace-nowrap flex justify-center items-center h-full gap-1 pt-3'>
+                    <img src="icon 2.png" alt="" class='h-3 w-3 sm:h-4 sm:w-4' />
+                    <div class='text-xs sm:text-sm text-gray-900'>{entry.total_points}</div>
                   </td>
                 </tr>
               ))}
             </tbody>
           </table>
-        )}
-
+        </div>
         {filteredEntry ? (
-          <div className='fixed w-[90%] mx-auto bottom-4 left-0 right-0 bg-[#4285F4] border-2 border-blue-500 rounded-full flex justify-between items-center px-4 py-2 shadow-xl'>
-            <div className='text-white'>
+          <div class='fixed w-[90%] max-w-xl mx-auto bottom-4 left-0 right-0 bg-[#4285F4] border-2 border-blue-500 rounded-full flex justify-between items-center px-2 py-1 sm:px-4 sm:py-2 shadow-xl'>
+            <div class='text-xs sm:text-sm text-white'>
               {filteredEntry.index}
               {filteredEntry.index === 0 ? 'st' : filteredEntry.index === 1 ? 'nd' : filteredEntry.index === 2 ? 'rd' : 'th'}
             </div>
-            <div className='text-white'>
+            <div class='text-xs sm:text-sm text-white'>
               {filteredEntry.territory_id}
             </div>
-            <div className='text-white flex justify-center items-center gap-1.5'>
-              <img src="icon 2.png" alt="" className='h-4 w-4' />
+            <div class='text-xs sm:text-sm text-white flex justify-center items-center gap-1'>
+              <img src="icon 2.png" alt="" class='h-3 w-3 sm:h-4 sm:w-4' />
               {filteredEntry.total_points}
             </div>
           </div>
         ) : (
-          <div className='fixed w-[90%] mx-auto bottom-4 left-0 right-0 bg-[#4285F4] border-2 border-gray-400 rounded-full flex justify-center items-center px-4 py-2 shadow-xl text-center'>
+          <div class='fixed w-[90%] mx-auto bottom-4 left-0 right-0 bg-[#4285F4] border-2 border-gray-400 rounded-full flex justify-center items-center px-2 py-1 sm:px-4 sm:py-2 shadow-xl text-center text-xs sm:text-sm'>
             Your Territory could not be found
           </div>
         )}
